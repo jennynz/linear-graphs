c1d5890c8385e875ac9f3f3786f5d6383420fb00
Jenny Sahng <jenny.s@hotmail.co.nz>
2018-03-01 00:32:49 +1300

    Redraw graph on event

--- src/index.js
+++ src/index.js
@@ -1,16 +1,59 @@
-import $ from 'jquery';
-import 'flot';
+import $ from "jquery";
+import "flot";
 import "./scss/style.scss";
 
-$(function() {
-  var d1 = [];
-  for (var i = 0; i < 14; i += 0.5) {
-    d1.push([i, Math.sin(i)]);
-  }
+$(document).ready(() => {
 
-  var d2 = [[0, 3], [4, 8], [8, 5], [9, 13]];
+  var data = [[0, 0], [10, 10]];
+  
+  var dataset = [
+    {
+      color: "#333",
+      data: data,
+      shadowSize: 0
+    }
+  ]
+  
+  var options = {
+    xaxis: {
+      min: -10,
+      max: 10,
+      // tickSize: [1, "month"],
+      // tickLength: 0,
+      // axisLabel: "2012",
+      // axisLabelUseCanvas: true,
+      // axisLabelFontSizePixels: 12,
+      // axisLabelFontFamily: "Verdana, Arial",
+      axisLabelPadding: 10
+    },
+    yaxis: {
+      min: -10,
+      max: 10
+    },
+    grid: {
+      borderWidth: 0
+    }
+  };
 
-  var d3 = [[0, 12], [7, 12], null, [7, 2.5], [12, 2.5]];
+  var graph = $.plot("#graph", dataset, options);
 
-  $.plot("#graph", [d1, d2, d3]);
+  $("#refresh").click(function() {
+
+    var dataset = graph.getData();
+    var line = dataset[0].data;
+    var x1 = line[0][0];
+    var y1 = line[0][1];
+    var x2 = line[1][0];
+    var y2 = line[1][1];
+
+    dataset[0].data = [
+      [x1 + 2, y1 + 2],
+      [x2 + 2, y2 + 2]
+    ];
+
+    console.log(dataset[0].data);
+    graph.setData(dataset);
+    graph.draw();
+  });
+
 });
